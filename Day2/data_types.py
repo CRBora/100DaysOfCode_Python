2cd52e5275b6aec68739b6dc2d04f3ae57997d6b
Llala <rene.raboshakga@gmail.com>
2023-10-24 21:07:48 +0200

    Updated code with task solve from instructor included as comments

--- Day2/data_types.py
+++ Day2/data_types.py
@@ -1,3 +1,21 @@
 two_digit_number = input()
 
 print(int(two_digit_number([0])) + int(two_digit_number([1])))
+
+# task solution from instructor
+###############################################
+
+# two_digit_number = input()
+# we first need to check input data type
+
+# print(type(two_digit_number))
+# <class 'str'>
+
+# next we index the characters we want to access in the string and type cast to an int
+# first_digit = int(two_digit_number[0]) -> for first character
+
+# second_digit = int(two_digit_number[1]) -> second character
+
+# two_digit_number = first_digit + second_digit
+
+# print(two_digit_number)
