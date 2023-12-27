80fd0253388938430721b1126a90e192c0cd8c1f
Llala <rene.raboshakga@gmail.com>
2023-12-27 12:43:18 +0200

    Added the original code I wrote as comments under the correct code

--- Day5/average_heights.py
+++ Day5/average_heights.py
@@ -7,13 +7,29 @@ for n in range(0, len(student_heights)):
   
 # Write your code below this row 👇
 
-student_num = len(student_heights)
 total_height = 0
 for n in student_heights:
   total_height += n
+print("total height = {}".format(total_height))
+
+student_num = 0
+for student in student_heights:
+  student_num += 1
+print("number of students = {}".format(student_num))
 
 avg_height = round(total_height/student_num)
 
-print("total height = {}".format(total_height))
-print("number of students = {}".format(student_num))
 print("average height = {}".format(avg_height))
+
+# My original code before correction acoording to instructions
+
+#studen_num = len(student_heights)
+#total_height = 0
+# for n in student_heights:
+#   total_height += n
+
+# avg_height = round(total_height/student_num)
+
+# print("total height = {}".format(total_height))
+# print("number of students = {}".format(student_num))
+# print("average height = {}".format(avg_height))
