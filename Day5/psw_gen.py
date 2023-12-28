e2adf3fa93c066300d2148e217c861f68c7112d3
Llala <rene.raboshakga@gmail.com>
2023-12-28 11:28:17 +0200

    Randomised password generator using for loops

---
+++ Day5/psw_gen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '+', ' ']
+
+print("Welcome to the Password Generator!")
+num_low_letters = int(input("How many lowercase letters would you like in your password?\n"))
+#num_up_letters = int(input("How many uppercase letters would you like in your password?\n"))
+num_symbols = int(input("How many symbols would you like in your password?\n"))
+num_numbers = int(input("How many numbers would you like in your password?\n"))
+
+#simpest solution
+#password = ""
+
+#for char in range(1, num_low_letters + 1):
+#    password += random.choice(letters)
+#for char in range(1, num_symbols + 1):
+#    password += random.choice(symbols)
+#for char in range(1, num_numbers + 1):
+ #   password += random.choice(numbers)
+#print(password)
+
+password_list = []
+
+for char in range(1, num_low_letters + 1):
+    password_list.append(random.choice(letters))
+for char in range(1, num_symbols + 1):
+    password_list.append(random.choice(symbols))
+for char in range(1, num_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+
+for char in password_list:
+    password += char
+
+print(password)
